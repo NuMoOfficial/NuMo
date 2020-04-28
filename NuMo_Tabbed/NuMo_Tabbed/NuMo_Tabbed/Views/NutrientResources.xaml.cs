5263524ca21b677bbf56af73bee5ea2576de1a0e
Dallas LeGrande <dallas.legrande@gmail.com>
2020-04-27 23:02:37 -0600

    added NutrientResource view. Need a way to navigate to it still

---
+++ NuMo_Tabbed/NuMo_Tabbed/NuMo_Tabbed/Views/NutrientResources.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NuMo_Tabbed.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NutrientResources : ContentPage
+    {
+        public NutrientResources()
+        {
+            InitializeComponent();
+            this.Title = "Resources on Nutrition";
+            BindingContext = this;
+        }
+        public ICommand ClickCommand2 => new Command<string>(async (url) =>
+        {
+            if (await Xamarin.Essentials.Launcher.CanOpenAsync(url))
+                await Xamarin.Essentials.Launcher.OpenAsync(new Uri(url));
+            else
+                await DisplayAlert("Alert", "The link was unable to open", "OK");
+        });
+    }
+}
